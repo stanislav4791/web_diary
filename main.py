b60ed49eb01b5f95daeb62b10e09e5bf34257f32
Tervasroso <82876191+Tervasroso@users.noreply.github.com>
2023-02-20 09:53:31 +0200

    database info updated

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'diary'
+app.config['MYSQL_HOST'] = 'sql7.freemysqlhosting.net'
+app.config['MYSQL_USER'] = 'sql7598384'
+app.config['MYSQL_PASSWORD'] = 'q4qIaKsMQW'
+app.config['MYSQL_DB'] = 'sql7598384'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
 
 mysql = MySQL(app)
@@ -25,20 +25,24 @@ def diary():
     return render_template("index.html", data = data, title='HOME')
 
 
-@app.route("/diary/create", methods=['POST', 'GET'])
+@app.route("/diary/create", methods=['GET','POST'])
 def insert():
+    print("PRINT")
     if request.method == ['GET']:
-        return render_template("create.html", title='CREATE')
-    if request.method == ['POST']:
+        print("Menee getiin")
+        return render_template("create.html", title="CREATE")
+    elif request.method == ['POST']:
+        print("Menee postiin")
         date = request.form['date']
         heading = request.form['heading']
         content = request.form['content']
         cur = mysql.connection.cursor()
         cur.execute("INSERT INTO entries (date, heading, content) VALUES (%s, %s, %s)", (date, heading, content))
         mysql.connection.commit()
-    return redirect("/diary")  
+        return redirect(url_for('diary'))
+
+    
     
         
-
 if __name__ == "__main__":
     app.run(debug=True)
