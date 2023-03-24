9c50c7f2aebef1f10a2aa08bbdec129bd35fb724
AnttiHirv <115528934+AnttiHirv@users.noreply.github.com>
2023-03-24 10:25:44 +0200

    created newsyle.py file

---
+++ newsyle3.py
@@ -0,0 +1,34 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+
+def ylenews_rss():
+    article_list = []
+
+    try:
+        r = requests.get('https://feeds.yle.fi/uutiset/v1/majorHeadlines/YLE_UUTISET.rss')
+        soup = BeautifulSoup(r.content, features='xml')
+        
+        articles = soup.findAll('item')
+
+        for a in articles:
+            title = a.find('title').text
+            link = a.find('link').text
+            description = a.find('description').text
+            
+
+            article = {
+                'title': title,
+                'link': link,
+                'description': description
+               }
+            article_list.append(article)
+
+        return article_list[0:5] 
+        
+    except Exception as e:
+        print('newsscrape failed.')
+        print(e)
+
+ylenews_rss()
+
